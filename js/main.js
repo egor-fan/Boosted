47addb50891c1a5377fcc79e789d2fcafef45bbd
egor-fan <juventini109@gmail.com>
2022-02-27 19:15:16 +0300

    end

--- js/main.js
+++ js/main.js
@@ -6,8 +6,47 @@ $(document).ready(function(){
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: true,
+    responsive: [
+    {
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 770,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    }
+  ]
   });
 });
+
+$(document).ready(function(){
+  $('.header__burger').click(function(event){
+    $('.header__burger, .menu__burger').toggleClass('active');
+    $('body').toggleClass('lock')
+    
+  })
+})
+
+$(document).ready(function(){
+  $('.header__list').click(function(event) {
+    $('.header__burger, .menu__burger').removeClass('active');
+    $('body').removeClass('lock');
+  })
+})
+
 
 var buttons = document.getElementsByClassName('btn'),
     forEach = Array.prototype.forEach;
